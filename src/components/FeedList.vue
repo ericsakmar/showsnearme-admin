02191227868da444f47e012d66e5ba6aea5b04cb
Eric Sakmar <eric.sakmar@gmail.com>
2017-02-02 22:20:27 -0500

    adds import buttons

--- src/components/FeedList.vue
+++ src/components/FeedList.vue
@@ -3,17 +3,21 @@
 
     <h1>Feeds</h1>
 
+    <p>{{message}}</p>
+
     <table>
       <thead>
         <tr>
           <th>Name</th>
           <th>ID</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="feed in feeds">
           <td>{{feed.name}}</td>
           <td>{{feed.remoteId}}</td>
+          <th><button @click.prevent="importFeed(feed.remoteId)">Import</button></th>
         </tr>
       </tbody>
     </table>
@@ -25,6 +29,22 @@
 export default {
   name: 'feed-list',
   props: ['feeds'],
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    importFeed(id) {
+      fetch(`http://localhost:3001/import/${id}`)
+        .then(res => res.json())
+        .then(res => this.showMessage(`Added ${res.added} shows`));
+    },
+    showMessage(message) {
+      this.message = message;
+      setTimeout(() => (this.message = ''), 5000);
+    },
+  },
 };
 </script>
 
